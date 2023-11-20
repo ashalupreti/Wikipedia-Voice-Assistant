d30c777fec1f73413bce580b358e121990cbba3b
Ashal Upreti <Upretiashal44@gmail.com>
2023-11-20 20:58:22 +0545

    Updated Google

--- Wikipedia Voice Assistant.py
+++ Wikipedia Voice Assistant.py
@@ -1,10 +1,21 @@
 import pyttsx3
 import wikipedia
 
-voice = pyttsx3.init()
-search_query = input("Searching wikipedia/google: ")
-result = wikipedia.summary(search_query)
-print(result)
-print(result)
-voice.say(result)
-voice.runAndWait()
+
+def search_wikipedia():
+    try:
+        voice = pyttsx3.init()
+        search_query = input("Searching wikipedia/google: ")
+        result = wikipedia.summary(search_query)
+        print(result)
+        voice.say(result)
+        voice.runAndWait()
+    except wikipedia.exceptions.DisambiguationError as e:
+        print("Please provide a more specific search query.")
+    except wikipedia.exceptions.PageError as e:
+        print("The page you requested does not exist.")
+    except Exception as e:
+        print("An error occurred while searching Wikipedia.")
+
+
+search_wikipedia()
